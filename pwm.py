f713c649046cb5bd4c412ff664e643d559b29664
Sun-Keeratawatana <61540826+Sun-Keeratawatana@users.noreply.github.com>
2021-02-17 22:22:57 +0700

    Add files via upload

---
+++ pwm.py
@@ -0,0 +1,12 @@
+import machine
+import utime
+
+Pwm = machine.PWM(machine.Pin(15))
+Pwm.freq(1000)
+for i in range(2):
+    for duty in range(65535):
+        Pwm.duty_u16(duty)
+    utime.sleep(1)
+    for duty in range(65536, -1, -1):
+        Pwm.duty_u16(duty)
+    utime.sleep(1)c
